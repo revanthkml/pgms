c907e4436a1de593b2c39ea671b5c7b638454a38
revanthkml <30474442+revanthkml@users.noreply.github.com>
2017-11-01 10:31:10 +0530

    Update numbers.java

--- numbers.java
+++ numbers.java
@@ -3,20 +3,20 @@ import java.util.io.*;
  {
  public static void main(String args[])
  {
- Printf("Enter a number to check"');
+ System.out.println("Enter a number to check"');
  Scanner a=new Scanner(System.in);
  int s=a.nextInt();
  if(s<0)
  {
- printf("The number"+s+"is negative);
+ System.out.println("The number"+s+"is negative);
  }
  else if(s>0)
  {
- printf("the number"+s+"is positive);
+ System.out.println("the number"+s+"is positive);
  }
  else
  {
- printf("the number"+s+"is zero);
+ System.out.println("the number"+s+"is zero);
  }
  }
  }
