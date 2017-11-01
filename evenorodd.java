adb2cbb90ae99d069b96d9eca3a1a7d7dea5db43
revanthkml <30474442+revanthkml@users.noreply.github.com>
2017-11-01 10:40:12 +0530

    Create evenorodd.java

---
+++ evenorodd.java
@@ -0,0 +1,17 @@
+class evenorodd
+{
+public static void main(String args[])
+{
+System.out.println("Enter a number to check");
+Scanner f=new Scanner(System.in);
+int x=f.nextInt();
+if(x&1)==0)
+{
+System.out.println("the number"+x+"is even");
+}
+else
+{
+System.out.println("the number"+x+"is odd");
+}
+}
+}
