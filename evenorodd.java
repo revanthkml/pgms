84831270ca4af792df63d2f0c3366ab8d0646b12
revanthkml <30474442+revanthkml@users.noreply.github.com>
2017-11-01 10:44:49 +0530

    Update evenorodd.java

--- evenorodd.java
+++ evenorodd.java
@@ -1,11 +1,12 @@
-class evenorodd
+import java.util.Scanner;
+public class evenorodd
 {
 public static void main(String args[])
 {
 System.out.println("Enter a number to check");
 Scanner f=new Scanner(System.in);
 int x=f.nextInt();
-if(x&1)==0)
+if((x&1)==0)
 {
 System.out.println("the number"+x+"is even");
 }
