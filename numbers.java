82b927a96eb08aced978c9106f2460fc16e2e86a
revanthkml <30474442+revanthkml@users.noreply.github.com>
2017-11-01 10:14:11 +0530

    numbers.java

---
+++ numbers.java
@@ -0,0 +1,22 @@
+import java.util.io.*;
+ class numbers
+ {
+ public static void main(String args[])
+ {
+ Printf("Enter a number to check"');
+ Scanner a=new Scanner(System.in);
+ int s=a.nextInt();
+ if(s<0)
+ {
+ printf("The number"+s+"is negative);
+ }
+ else if(s>0)
+ {
+ printf("the number"+s+"is positive);
+ }
+ else
+ {
+ printf("the number"+s+"is zero);
+ }
+ }
+ }
